2ccb7e45147814dbf9d89ead3cfecd8dfb644a65
Nana <anna021210@outlook.com>
2026-05-05 23:48:17 +0800

    uqpe

--- src/components/HelpMenu.tsx
+++ src/components/HelpMenu.tsx
@@ -1,24 +1,10 @@
 import { ButtonMenu, CloseIcon } from "./ButtonMenu";
-import lineL from "../assets/Modal/lineL.svg"
-import lineM from "../assets/Modal/lineM.svg"
-import lineR from "../assets/Modal/lineR.svg"
-import ring from "../assets/Body/element/ring.svg"
-import peach from "../assets/Body/element/peach.svg"
-import cherry from "../assets/Body/element/cherry.svg"
-import diamond from "../assets/Body/element/diamond.svg"
-import seven from "../assets/Body/element/seven.svg"
-import grape from "../assets/Body/element/grape.svg"
-import watermelon from "../assets/Body/element/watermelon.svg"
-import one from "../assets/Modal/1.svg"
-import zero from "../assets/Modal/0.svg"
-import three from "../assets/Modal/3.svg"
-import five from "../assets/Modal/5.svg"
-
+import { useGame, resolveAssetUrl } from "../hooks/useGameHook";
 type HelpMenuProps = {
     onCloseHelpModal: () => void;
 };
-
 export default function HelpMenu({ onCloseHelpModal }: HelpMenuProps) {
+    const { options, } = useGame()
     return (
         <div className="h-[274px] bg-gradient-to-t from-[#5028C1] to-[#7C00D5] w-[393px] rounded-t-[10px]">
             <span className="absolute flex left-[151px] top-[18px] text-[24px]  font-semibold">Class Slot</span>
@@ -32,11 +18,38 @@ export default function HelpMenu({ onCloseHelpModal }: HelpMenuProps) {
                 />
             </div>
             <div className="absolute flex bg-[#000000]/25 top-[11px] left-[15px] h-[74px] w-[122px] rounded-[5px]">
-                <img src={lineL} alt="Line Left" className="absolute top-[12px] left-[23px] w-[78px] h-[52px]" />
-                <img src={lineM} alt="Line Middle" className="absolute top-[12px] left-[23px] w-[78px]" />
-                <img src={lineM} alt="Line Middle" className="absolute top-[37px] left-[23px] w-[78px]" />
-                <img src={lineM} alt="Line Middle" className="absolute top-[62px] left-[23px] w-[78px]" />
-                <img src={lineR} alt="Line Right" className="absolute top-[12px] left-[23px] w-[78px] h-[52px]" />
+                <svg>
+                    <path
+                        d="M19 10 L102 65 "
+                        stroke="white"
+                        strokeWidth="3"
+                        fill="transparent"
+                    />
+                    <path
+                        d="M102 10 L19 65 "
+                        stroke="white"
+                        strokeWidth="3"
+                        fill="transparent"
+                    />
+                    <path
+                        d="M18 12 L95 12 "
+                        stroke="white"
+                        strokeWidth="3"
+                        fill="transparent"
+                    />
+                    <path
+                        d="M18 37 L95 37 "
+                        stroke="white"
+                        strokeWidth="3"
+                        fill="transparent"
+                    />
+                    <path
+                        d="M18 62 L95 62 "
+                        stroke="white"
+                        strokeWidth="3"
+                        fill="transparent"
+                    />
+                </svg>
                 <div className="absolute top-[7px] left-[18px] w-[10px] h-[10px] rounded-full bg-[#FFFFFF]"></div>
                 <div className="absolute top-[7px] left-[56px] w-[10px] h-[10px] rounded-full bg-[#FFFFFF]"></div>
                 <div className="absolute top-[7px] left-[94px] w-[10px] h-[10px] rounded-full bg-[#FFFFFF]"></div>
@@ -49,96 +62,70 @@ export default function HelpMenu({ onCloseHelpModal }: HelpMenuProps) {
             </div>
             <div className="absolute top-[91px] left-[12px] grid grid-cols-2 grid-rows-4 gap-[10px]">
                 <div className="relative flex bg-[#000000]/25 h-[34px] w-[180px] rounded-[5px] items-center ">
-                    <img src={ring} alt="Ring" className="relative  left-[5px] w-[24px] h-[24px]" />
-                    <img src={ring} alt="Ring" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={ring} alt="Ring" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[0].logo)} alt="Ring" className="relative  left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[0].logo)} alt="Ring" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[0].logo)} alt="Ring" className="relative left-[5px] w-[24px] h-[24px]" />
                     <span className="relative left-[8px] top-[3px] w-[24px] h-[24px]">=</span>
-                    <img src={one} alt="Ring" className="relative pl-[5px] w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
+                    <span className="absolute left-[85px] "
+                        style={{ fontFamily: "MyBoldFont", letterSpacing: "0px", fontSize: "16px", color: "gold" }}>{1000000}</span>
                 </div>
                 <div className="relative flex bg-[#000000]/25 h-[34px] w-[180px] rounded-[5px]  items-center">
-                    <img src={seven} alt="Seven" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={seven} alt="Seven" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={seven} alt="Seven" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[1].logo)} alt="Seven" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[1].logo)} alt="Seven" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[1].logo)} alt="Seven" className="relative left-[5px] w-[24px] h-[24px]" />
                     <span className="relative left-[8px] top-[3px] w-[24px] h-[24px]">=</span>
-                    <img src={three} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
+                    <span className="absolute left-[89px] "
+                        style={{ fontFamily: "MyBoldFont", letterSpacing: "1px", fontSize: "16px", color: "gold" }}>{300000}</span>
                 </div>
                 <div className="relative flex bg-[#000000]/25 h-[34px] w-[180px] rounded-[5px] items-center">
-                    <img src={diamond} alt="Diamond" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={diamond} alt="Diamond" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={diamond} alt="Diamond" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[2].logo)} alt="Diamond" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[2].logo)} alt="Diamond" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[2].logo)} alt="Diamond" className="relative left-[5px] w-[24px] h-[24px]" />
                     <span className="relative left-[8px] top-[3px] w-[24px] h-[24px]">=</span>
-                    <img src={one} alt="Ring" className="relative pl-[5px] w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
+                    <span className="absolute left-[85px] "
+                        style={{ fontFamily: "MyBoldFont", letterSpacing: "2px", fontSize: "16px", color: "gold" }}>{100000}</span>
                 </div>
                 <div className="relative flex bg-[#000000]/25 h-[34px] w-[180px] rounded-[5px] items-center">
-                    <img src={watermelon} alt="Watermelon" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={watermelon} alt="Watermelon" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={watermelon} alt="Watermelon" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[3].logo)} alt="Watermelon" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[3].logo)} alt="Watermelon" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[3].logo)} alt="Watermelon" className="relative left-[5px] w-[24px] h-[24px]" />
                     <span className="relative left-[8px] top-[3px] w-[24px] h-[24px]">=</span>
-                    <img src={five} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
+                    <span className="absolute left-[90px] "
+                        style={{ fontFamily: "MyBoldFont", letterSpacing: "3px", fontSize: "16px", color: "gold" }}>{50000}</span>
                 </div>
                 <div className="relative flex bg-[#000000]/25 h-[34px] w-[180px] rounded-[5px] items-center">
-                    <img src={grape} alt="Grape" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={grape} alt="Grape" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={grape} alt="Grape" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[6].logo)} alt="Grape" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[6].logo)} alt="Grape" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[6].logo)} alt="Grape" className="relative left-[5px] w-[24px] h-[24px]" />
                     <span className="relative left-[8px] top-[3px] w-[24px] h-[24px]">=</span>
-                    <img src={three} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
+                    <span className="absolute left-[90px] "
+                        style={{ fontFamily: "MyBoldFont", letterSpacing: "3px", fontSize: "16px", color: "gold" }}>{30000}</span>
                 </div>
                 <div className="relative flex bg-[#000000]/25 h-[34px] w-[180px] rounded-[5px] items-center">
-                    <img src={peach} alt="Peach" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={peach} alt="Peach" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={peach} alt="Peach" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[5].logo)} alt="Peach" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[5].logo)} alt="Peach" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[5].logo)} alt="Peach" className="relative left-[5px] w-[24px] h-[24px]" />
                     <span className="relative left-[8px] top-[3px] w-[24px] h-[24px]">=</span>
-                    <img src={one} alt="Ring" className="relative pl-[5px] w-[18px] h-[18px]" />
-                    <img src={five} alt="Ring" className="relative w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
+                    <span className="absolute left-[90px] "
+                        style={{ fontFamily: "MyBoldFont", letterSpacing: "3px", fontSize: "16px", color: "gold" }}>{15000}</span>
                 </div>
                 <div className="relative flex bg-[#000000]/25 h-[34px] w-[180px] rounded-[5px] items-center">
-                    <img src={cherry} alt="Cherry" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={cherry} alt="Cherry" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={cherry} alt="Cherry" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[4].logo)} alt="Cherry" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[4].logo)} alt="Cherry" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[4].logo)} alt="Cherry" className="relative left-[5px] w-[24px] h-[24px]" />
                     <span className="relative left-[8px] top-[3px] w-[24px] h-[24px]">=</span>
-                    <img src={five} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
+                    <span className="absolute left-[95px] "
+                        style={{ fontFamily: "MyBoldFont", letterSpacing: "3px", fontSize: "16px", color: "gold" }}>{5000}</span>
                 </div>
                 <div className="relative flex bg-[#000000]/25 h-[34px] w-[180px] rounded-[5px] items-center">
-                    <img src={cherry} alt="Cherry" className="relative left-[5px] w-[24px] h-[24px]" />
-                    <img src={cherry} alt="Cherry" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[4].logo)} alt="Cherry" className="relative left-[5px] w-[24px] h-[24px]" />
+                    <img src={resolveAssetUrl(options[4].logo)} alt="Cherry" className="relative left-[5px] w-[24px] h-[24px]" />
                     <div className="relative left-[5px] w-[24px] h-[24px] bg-[#FFFFFF]/20 font-[#FFFFFF] text-[7px] text-white pl-[4px] content-center">ANY</div>
                     <span className="relative left-[8px] top-[3px] w-[24px] h-[24px]">=</span>
-                    <img src={five} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
-                    <img src={zero} alt="Ring" className="relative  w-[18px] h-[18px]" />
+                    <span className="absolute left-[95px] "
+                        style={{ fontFamily: "MyBoldFont", letterSpacing: "3px", fontSize: "16px", color: "gold" }}>{5000}</span>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
