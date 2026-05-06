e5d00abd05b6e6ee73726787559a3872fd9bb29f
Nana <anna021210@outlook.com>
2026-05-06 10:14:25 +0800

    u

--- src/components/LoadingScrean.tsx
+++ src/components/LoadingScrean.tsx
@@ -19,7 +19,7 @@ export default function LoadingScreen({
             className=" h-svh w-full text-left bg-[#41008b]"
         >
             <div className="absolute  inset-0 z-[200] flex-col flex left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-[300px] w-[200px] rounded-[40px] border-blue-950 items-center justify-center px-6">
-                <div className="w-[100px] h-[100px] ">
+                <div className="w-[150px] h-[150px] ">
                     <img src={getAssetUrl(GAME_ASSETS.loadingLogo)} alt="Loading" />
                 </div>
                 <div className="h-[16px] w-full bg-gray-300/50 rounded-full">
